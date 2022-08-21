ee12717ffc648ca54914804f3cce10a595a5550e
vmalv <110790512+vmalv@users.noreply.github.com>
2022-08-21 16:03:24 -0400

    Improved docstring and comments in crop_raw function in libs/utils.py

--- libs/utils.py
+++ libs/utils.py
@@ -10,10 +10,10 @@ def crop_raw(image: Image, length: float, v_shift: int, h_shift: int, dpi: int) 
 
     Arguments:
         image -- PIL.Image object containing the raw image.
-        length -- Length and width of scanned squared area in centimeters.
-        v_shift -- The number of pixels to shift the image on the vertical axis
-        h_shift -- The number of pixels to shift the image on the horizontal axis
-        dpi -- Dots per inch (DPI) used to scan the droplet patterns
+        length -- Desired width and height of final image in centimeters (side length of scanned squared area in the experiments)
+        v_shift -- The number of pixels to shift the image on the vertical axis (downward direction)
+        h_shift -- The number of pixels to shift the image on the horizontal axis (right direction)
+        dpi -- Dots per inch (DPI) of raw image (resolution used to scan the droplet patterns)
 
     Raises:
         Exception: Error cropping the image.
@@ -24,19 +24,19 @@ def crop_raw(image: Image, length: float, v_shift: int, h_shift: int, dpi: int) 
 
     width, height = image.size   # Get dimensions of original image
 
-    new_width = dpi/2.54 * length   # Get the desired width
-    new_height = dpi/2.54 * length  # Get the desired height
+    new_width = dpi/2.54 * length   # Set the desired width
+    new_height = dpi/2.54 * length  # Set the desired height
 
-    left = (width - new_width)/2 + h_shift       # Get the left pixel coordinate 
-    top = (height - new_height)/2 + v_shift      # Get the top pixel coordinate
-    right = (width + new_width)/2 + h_shift      # Get the right pixel coordinate
-    bottom = (height + new_height)/2 + v_shift   # Get the bottom pixel coordinate
+    left = (width - new_width)/2 + h_shift       # Set the left pixel coordinate 
+    top = (height - new_height)/2 + v_shift      # Set the top pixel coordinate
+    right = (width + new_width)/2 + h_shift      # Set the right pixel coordinate
+    bottom = (height + new_height)/2 + v_shift   # Set the bottom pixel coordinate
 
     # Validate the new pixel coordinates.
     if left<0 or right>width or top<0 or bottom>height:
         print(' width: {}\n height: {}'.format(width, height))
         print(' new_left: {}\n new_top: {}\n new_right: {}\n new_bottom: {}'.format(left, top, right, bottom))
-        raise Exception('Error cropping the image. Check the shift values printed above, the image borders are exceeded.') 
+        raise Exception('Error cropping the image. Check the shift values printed above. The image borders are exceeded.') 
 
     # Crop the image to the desired dimensions
     image_cut = image.crop((left, top, right, bottom))   # Crop the center of the image

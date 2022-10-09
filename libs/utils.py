535f3ebea78987fccb8b3da78a0609422a58bbc6
Jorge Morgado Vega <jorge.morgadov@gmail.com>
2022-10-09 03:03:22 -0400

    Fix code and change docstring style

--- libs/utils.py
+++ libs/utils.py
@@ -1,59 +1,95 @@
 import numpy as np
-from PIL import Image
+from PIL.Image import Image
 
 
-def crop_raw(image: Image, length: float, v_shift: int, h_shift: int, dpi: int) -> Image:
-    """Crops the original raw image.
+def crop_raw(
+    image: Image,
+    length: float,
+    v_shift: int,
+    h_shift: int,
+    dpi: int,
+) -> Image:
+    """
+    Crops the original raw image.
 
     Crops and shifts the original raw image removing the borders.
 
-    Arguments:
-        image -- PIL.Image object containing the raw image.
-        length -- Desired width and height of final image in centimeters (side length of scanned squared area in the experiments)
-        v_shift -- The number of pixels to shift the image on the vertical axis (downward direction)
-        h_shift -- The number of pixels to shift the image on the horizontal axis (right direction)
-        dpi -- Dots per inch (DPI) of raw image (resolution used to scan the droplet patterns)
+    Parameters
+    ----------
+    image : Image
+        PIL.Image object containing the raw image.
+    length : float
+        Desired width and height of final image in centimeters (side
+        length of scanned squared area in the experiments).
+    v_shift : int
+        The number of pixels to shift the image on the vertical
+        axis (downward direction).
+    h_shift : int
+        The number of pixels to shift the image on the horizontal
+        axis (right direction).
+    dpi : int
+        Dots per inch (DPI) of raw image (resolution used to scan
+        the droplet patterns).
 
-    Raises:
-        Exception: Error cropping the image.
+    Raises
+    ------
+    Exception
+        If the image cannot be cropped to the desired dimensions.
 
-    Returns:
+    Returns
+    -------
+    Image
         Cropped image with the desired dimensions.
     """
 
-    width, height = image.size   # Get dimensions of original image
+    # Get dimensions of original image
+    width, height = image.size
 
-    new_width = dpi/2.54 * length   # Set the desired width
-    new_height = dpi/2.54 * length  # Set the desired height
+    # Set desired dimensions of cropped image
+    new_width = dpi / 2.54 * length
+    new_height = dpi / 2.54 * length
 
-    left = (width - new_width)/2 + h_shift       # Set the left pixel coordinate 
-    top = (height - new_height)/2 + v_shift      # Set the top pixel coordinate
-    right = (width + new_width)/2 + h_shift      # Set the right pixel coordinate
-    bottom = (height + new_height)/2 + v_shift   # Set the bottom pixel coordinate
+    # Set boundaries pixel coordinates
+    left = int((width - new_width) / 2 + h_shift)
+    top = int((height - new_height) / 2 + v_shift)
+    right = int((width + new_width) / 2 + h_shift)
+    bottom = int((height + new_height) / 2 + v_shift)
 
     # Validate the new pixel coordinates.
-    if left<0 or right>width or top<0 or bottom>height:
-        print(' width: {}\n height: {}'.format(width, height))
-        print(' new_left: {}\n new_top: {}\n new_right: {}\n new_bottom: {}'.format(left, top, right, bottom))
-        raise Exception('Error cropping the image. Check the shift values printed above. The image borders are exceeded.') 
+    if left < 0 or right > width or top < 0 or bottom > height:
+        print(f" width: {width}", f"height: {height}", sep="\n")
+        print(
+            f" new_left: {left}",
+            f" new_top: {top}",
+            f" new_right: {right}",
+            f" new_bottom: {bottom}",
+            sep="\n",
+        )
+        raise Exception(
+            "Error cropping the image. Check the shift values printed above. "
+            "The image borders are exceeded."
+        )
 
-    # Crop the image to the desired dimensions
-    image_cut = image.crop((left, top, right, bottom))   # Crop the center of the image
-
-    return image_cut
+    # Returns the image cropped to the desired dimensions
+    return image.crop((left, top, right, bottom))
 
 
 def get_p(binaryimage: Image) -> None:
-    """Extracts the mean pixel value (p) of a binary image.
-    
-    Extracts pixel values matrix from a binary image, flattens the matrix to a single list and finds its mean.
-    
-    The parameter p corresponds to both the parameters <\overline{p_{o}}> (Latex formatting) and <\overline{p_{no}}>, since both are calculated the same way. Please refer to the article to know their difference and usage.
+    """
+    Extracts the mean pixel value (p) of a binary image.
 
-    Argument:
-        binaryimage -- Binarized image. 
+    Extracts pixel values matrix from a binary image, flattens the matrix to a
+    single list and finds its mean.
+
+    The parameter p corresponds to both the parameters <\\overline{p_{o}}>
+    (Latex formatting) and <\\overline{p_{no}}>, since both are calculated the
+    same way. Please refer to the article to know their difference and usage.
+
+    Parameters
+    ----------
+    binaryimage : Image
+        Binarized image.
     """
 
     p = np.mean(np.array(binaryimage).flatten())
-    
-    print('p: {}'.format(p))
+    print(f"p: {p}")
--- main.py
+++ main.py
@@ -1,33 +1,32 @@
-from PIL import Image
-import PIL.ImageOps
-import os
+from pathlib import Path
+
+from PIL import Image, ImageOps
 
 from libs import utils
 
-
 ### Image parameters
 
-# Dots per inch (DPI) of raw image (resolution used to scan the droplet 
+# Dots per inch (DPI) of raw image (resolution used to scan the droplet
 # patterns).
 dpi = 3200
 
-# Desired width and height of final image in centimeters (side length of 
+# Desired width and height of final image in centimeters (side length of
 # scanned squared area in the experiments).
 length = 5.0
 
-# Binary threshold used to binarize the images. 0.3 gives the most accurate 
-# results with our materials, but any other value can be used. 
+# Binary threshold used to binarize the images. 0.3 gives the most accurate
+# results with our materials, but any other value can be used.
 binary_threshold = 0.3
 
-# By default, the crop_raw function defined in libs/utils.py crops the center 
-# of the raw image, discarding the same amount of pixels on its left and right 
-# sides, as well as on its top and bottom sides. 
-# Sometimes, however, the cropped image contains elements that are not desired. 
+# By default, the crop_raw function defined in libs/utils.py crops the center
+# of the raw image, discarding the same amount of pixels on its left and right
+# sides, as well as on its top and bottom sides.
+# Sometimes, however, the cropped image contains elements that are not desired.
 # For example, the reference marks printed on our experiment paper sheets.
-# These two parameters allow us to shift the region that will be cropped. They 
-# represent the number of pixels to shift the cropped image on the vertical and 
+# These two parameters allow us to shift the region that will be cropped. They
+# represent the number of pixels to shift the cropped image on the vertical and
 # horizontal axis, i.e. on the downward and right directions.
-# It is recommended to start with both parameters equal to 0, and adjust them 
+# It is recommended to start with both parameters equal to 0, and adjust them
 # accordingly only if neccesary. It is also recommended to use the 'example.
 # ipynb' file to refine the parameters values by trial and error method.
 v_shift = 0
@@ -41,63 +40,61 @@ file = "TelaAzul_Conf1_Exp1_3200dpi"
 ext = ".bmp"
 
 # Load raw image from the 'input/' folder
-raw = Image.open(os.path.join(os.path.dirname(__file__),'input', file+ext))
+input_file_path = str(Path(__file__).parent / "input" / (file + ext))
+raw = Image.open(input_file_path)
 
 
 ### Crop the raw image
 
-# The cropped image size is determined by the length parameter defined above, 
-# so that the image will be (length x length) cm^2. For example, if length==5.0 
-# the cropped image will be 5x5 cm^2. 
-# Check that the cropped image does not contain undesired elements. In our 
-# experiments, no portion of the reference marks printed on the paper should 
-# remain included inside the cropped image. 
-# Remember that after cropping we keep the center of the original image, i.e. 
-# borders are removed symmetrically until specified dimensions are satisfied. 
+# The cropped image size is determined by the length parameter defined above,
+# so that the image will be (length x length) cm^2. For example, if length==5.0
+# the cropped image will be 5x5 cm^2.
+# Check that the cropped image does not contain undesired elements. In our
+# experiments, no portion of the reference marks printed on the paper should
+# remain included inside the cropped image.
+# Remember that after cropping we keep the center of the original image, i.e.
+# borders are removed symmetrically until specified dimensions are satisfied.
 # For further details see crop_raw definition in libs/utils.py.
 rawcut = utils.crop_raw(raw, length, v_shift, h_shift, dpi)
 
 
 ### Select the RGB component with best stains-background contrast
 
-# In our experiments the stains are blue and background is white, so the red 
-# component of the image negative gives the best contrast between stains and 
-# background. Then, we find the cropped image negative and extract its first 
+# In our experiments the stains are blue and background is white, so the red
+# component of the image negative gives the best contrast between stains and
+# background. Then, we find the cropped image negative and extract its first
 # (red) component in the RGB color space.
 # In case of using another color for stains and/or background, select the
 # component that maximizes the contrast between them.
-rawneg = PIL.ImageOps.invert(rawcut)
+rawneg = ImageOps.invert(rawcut)
 rawnegred, _, _ = rawneg.split()
 
 
 ### Binarize the RGB component
 
 # Binarize 'rawnegred' image using the parameter 'binary_threshold'.
-# All pixels values lower than 'binary_threshold' are set to 0 (so pixels are 
-# turned to black pixels). Otherwise, they are set to 255 (so pixels turn to 
+# All pixels values lower than 'binary_threshold' are set to 0 (so pixels are
+# turned to black pixels). Otherwise, they are set to 255 (so pixels turn to
 # white).
 
 # With the Image.point() method from the Pillow library we are able to map
 # each pixel through a function.
-# 'rawnegred' is a grayscale image. The Pillow library assigns pixels in a 
-# grayscale image a value from 0 to 255. So we must rescale 'binary_threshold' 
+# 'rawnegred' is a grayscale image. The Pillow library assigns pixels in a
+# grayscale image a value from 0 to 255. So we must rescale 'binary_threshold'
 # to the 0-255 scale.
-binary = rawnegred.point(lambda p: 255 if p >= (binary_threshold*255) else 0)
+binary = rawnegred.point(lambda p: 255 if p >= (binary_threshold * 255) else 0)
 
-# We turn 'binary' into a binary image, that is, its pixels can only have two 
+# We turn 'binary' into a binary image, that is, its pixels can only have two
 # values: '0' (Black) or '1' (White).
 binary = binary.convert("1")
 
-# 'binary' image is exported to the 'output/' folder, in PNG format (to save 
-# storage) and with a dpi in each direction equal to the 'dpi' parameter 
+# 'binary' image is exported to the 'output/' folder, in PNG format (to save
+# storage) and with a dpi in each direction equal to the 'dpi' parameter
 # defined above.
-binary.save(os.path.join(os.path.dirname(__file__),'output', 'Binary_' + file + '.png'), 'PNG', dpi=(dpi, dpi))
-# A message is printed notifying that the image was exported.
-print('Binarized image exported to the file: {}'.format('output/' + 'Binary_' + file + '.png'))
-
+output_file_path = str(Path(__file__).parent / "output" / f"Binary_{file}.png")
+binary.save(output_file_path, "PNG", dpi=(dpi, dpi))
+print(f"Binarized image exported to the file: {output_file_path}")
 
 ### Get the binary image mean pixel value
 
-# Use the get_p function defined at libs/utils.py 
-# to get the mean pixel value (p) of the 'binary' image.
 utils.get_p(binary)
